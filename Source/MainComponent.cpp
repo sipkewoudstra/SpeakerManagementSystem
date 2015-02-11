6bc5664babfd17d57963ae289a3bef8446c18a62
sipkewoudstra <sipkewoudstra@gmail.com>
2015-02-11 22:30:14 +0100

    First Commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,106 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "Dsp.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public AudioAppComponent
+{
+public:
+    //==============================================================================
+    MainContentComponent()
+    {
+        setSize (800, 600);
+
+        // specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+
+    ~MainContentComponent()
+    {
+        shutdownAudio();
+    }
+
+    //=======================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+
+            LP1 = new Dsp::SmoothedFilterDesign
+            <Dsp::Butterworth::Design::LowPass<8>, 2, Dsp::DirectFormII<float>> (1024);
+        
+            HP1 = new Dsp::SmoothedFilterDesign
+            <Dsp::Butterworth::Design::HighPass<8>, 2, Dsp::DirectFormII<float>> (1024);
+        
+            lp1Params[0] = sampleRate; // sample rate
+            lp1Params[1] = 1; // order
+            lp1Params[2] = 20000; // center frequency
+            LP1->setParams (lp1Params);
+            //f->process (numSamples, audioData);
+    }
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
+    {
+        // Your audio-processing code goes here!
+
+        // For more details, see the help for AudioProcessor::getNextAudioBlock()
+    }
+
+    void releaseResources() override
+    {
+        // This will be called when the audio device stops, or when it is being
+        // restarted due to a setting change.
+
+        // For more details, see the help for AudioProcessor::releaseResources()
+    }
+
+    //=======================================================================
+    void paint (Graphics& g) override
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (Colours::black);
+
+
+        // You can add your drawing code here!
+    }
+
+    void resized() override
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+    }
+
+
+private:
+    //==============================================================================
+
+    // Your private member variables go here...
+    Dsp::Filter* LP1;
+    Dsp::Filter* HP1;
+    
+    Dsp::Params lp1Params;
+    Dsp::Params hp1Params;
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+
+// (This function is called by the app startup code to create our main component)
+Component* createMainContentComponent()     { return new MainContentComponent(); }
+
+
+#endif  // MAINCOMPONENT_H_INCLUDED
